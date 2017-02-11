9e30535039c7882e9e8aab02713424ab618261c7
syoo15 <syoo15@illinois.edu>
2017-02-10 18:51:27 -0600

    BLE version working. Need to work on UART screen and communication setup

---
+++ Application/src/main/java/com/example/android/bluetoothlegatt/DialogUtils.java
@@ -0,0 +1,21 @@
+package com.example.android.bluetoothlegatt;
+
+import android.app.Dialog;
+import android.view.WindowManager;
+
+/**
+ * Created by SeokChan on 2017-02-10.
+ */
+
+public class DialogUtils {
+
+    // Prevent dialog dismiss when orientation changes
+    // http://stackoverflow.com/questions/7557265/prevent-dialog-dismissal-on-screen-rotation-in-android
+    public static void keepDialogOnOrientationChanges(Dialog dialog){
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        lp.copyFrom(dialog.getWindow().getAttributes());
+        lp.width = WindowManager.LayoutParams.WRAP_CONTENT;
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        dialog.getWindow().setAttributes(lp);
+    }
+}
